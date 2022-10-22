246ad3e944b2a4fbcfa411fa00ae04645792c865
sammite <54330632+sammite@users.noreply.github.com>
2022-10-22 07:37:07 -0700

    added kernel forwarding

--- kernel-update.sh
+++ kernel-update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# update the kernel so that it will forward packets.
 
+# THIS IS NOT SECURE IN ITS CURRENT VERSION
+# DO NOT RUN ON LIVE NETWORK
+# SAFETY CHECKS TO BE ADDED
+# MUST BE RUN AS SUDO
+# update the kernel so that it will forward packets.
+sysctl -w net.ipv4.ip_forward=1
+
+# make it persistent
+
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
